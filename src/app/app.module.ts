9f8dee69db4391b0b3dbee89c937099ddca653a7
Ranjan <jyotiranjan.p14@gmail.com>
2019-06-01 20:31:28 +0530

    Custom Attribute Directive Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms";
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { StringInterpolationComponent } from './components/string-interpolation/string-interpolation.component';
-import { PropertyBindingComponent } from './components/property-binding/property-binding.component';
-import { EventBindingComponent } from './components/event-binding/event-binding.component';
-import { StructDirectivesComponent } from './components/struct-directives/struct-directives.component';
-import { ProductThumbnailComponent } from './components/product-thumbnail/product-thumbnail.component';
+import {
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductListComponent,
+    StringInterpolationComponent,
+    PropertyBindingComponent,
+    EventBindingComponent,
+    StructDirectivesComponent,
+    ProductThumbnailComponent
+} from "./components/component.index";
+
 import { ShortenPipe } from "./pipes/shorten.pipe";
 import { ConvertToPipe } from './pipes/convert-to.pipe';
 import { FilterProductPipe } from './pipes/filter-product.pipe';
+import {BasicHighlightDirective} from "./directives/basic-highlight.directive";
 
 @NgModule({
   declarations: [
@@ -28,7 +32,8 @@ import { FilterProductPipe } from './pipes/filter-product.pipe';
     ProductThumbnailComponent,
     ShortenPipe,
     ConvertToPipe,
-    FilterProductPipe
+    FilterProductPipe,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/component.index.ts
@@ -0,0 +1,10 @@
+
+export { AppComponent } from '../app.component';
+export { HeaderComponent } from './header/header.component';
+export { FooterComponent } from './footer/footer.component';
+export { ProductListComponent } from './product-list/product-list.component';
+export { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
+export { PropertyBindingComponent } from './property-binding/property-binding.component';
+export { EventBindingComponent } from './event-binding/event-binding.component';
+export { StructDirectivesComponent } from './struct-directives/struct-directives.component';
+export { ProductThumbnailComponent } from './product-thumbnail/product-thumbnail.component';
